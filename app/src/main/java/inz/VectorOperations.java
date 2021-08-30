58bc2479b361de1989ac9680c0a783fb4e7ebfea
Etav99 <etavpl@gmail.com>
2021-08-30 17:40:55 +0200

    Add class for basic operations on vectors.

---
+++ app/src/main/java/inz/VectorOperations.java
@@ -0,0 +1,73 @@
+package inz;
+
+import 	java.lang.Math;
+
+public final class VectorOperations {
+
+    /**
+     * Normalize vector by it's l2 norm (unit vector)
+     *
+     * @param vector vector to normalize
+     * @return normalized vector (unit vector)
+     * @exception NullPointerException when null vector passed.
+     */
+    final public float[] l2Normalize(float vector[]){
+        if(vector == null)
+            throw new NullPointerException("Null vector passed");
+
+        float normalizedVector[] = new float[vector.length];
+        double div = 0;
+        for (float item: vector) {
+            div += item*item;
+        }
+        double sqrt = Math.sqrt(div);
+        for(int i = 0; i < vector.length; i++){
+            normalizedVector[i] = (float)(vector[i]/div);
+        }
+        return normalizedVector;
+    }
+
+    /**
+     * Calculate euclidean distance between two vectors.
+     *
+     * @param vector1 first vector
+     * @param vector2 second vector
+     * @return euclidean distance between the two vectors
+     * @exception NullPointerException when null vector(s) passed.
+     */
+    final public double euclideanDistance(float vector1[], float vector2[]){
+        if(vector1 == null || vector2 == null)
+            throw new NullPointerException("Null vector(s) passed");
+
+        double distance = 0;
+        for(int i = 0; i < vector1.length; i++){
+            double diff = vector1[i] - vector2[i];
+            distance += diff*diff;
+        }
+        return Math.sqrt(distance);
+    }
+
+    /**
+     * Calculate cosine similarity between vectors
+     *
+     * @param vector1 first vector
+     * @param vector2 second vector
+     * @return cosine similarity between the two vectors
+     * @exception NullPointerException when null vector(s) passed.
+     */
+    final public double cosineSimilarity(float vector1[], float vector2[]){
+        if(vector1 == null || vector2 == null)
+            throw new NullPointerException("Null vector(s) passed");
+
+        double a = 0, b = 0, c = 0;
+        for(int i = 0; i < vector1.length; i++){
+            a += vector1[i] * vector2[i];
+            b += vector1[i] * vector1[i];
+            c += vector2[i] * vector2[i];
+        }
+        b = Math.sqrt(b);
+        c = Math.sqrt(c);
+        return 1 - (a / (b * c));
+    }
+
+}

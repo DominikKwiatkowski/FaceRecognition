44263074ca82f279e0c6f5132f953b76e79ae90e
Dominik Kwiatowski <domik.rex@gmail.com>
2021-08-31 18:37:32 +0200

    Make face detection and recognition work asynchronously.

--- app/src/main/java/com/CameraActivity.java
+++ app/src/main/java/com/CameraActivity.java
@@ -22,6 +22,8 @@ import org.opencv.imgproc.Imgproc;
 import org.tensorflow.lite.support.image.TensorImage;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
 
@@ -31,6 +33,11 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     static {
         System.loadLibrary("opencv_java3");
     }
+
+    Executor singleThreadExecutor = Executors.newSingleThreadExecutor();
+    MatOfRect faceMat = null;
+    String [] names = null;
+    Mat lastFrame = null;
 
     /**
      * Method to get and set stuff after view creation.
@@ -53,7 +60,6 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
         mOpenCvCameraView = findViewById(R.id.java_surface_view);
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setVisibility(View.VISIBLE);
-        //mOpenCvCameraView.setCameraIndex(1);
 
         // Neural model load.
         model = new NeuralModel(this, "Facenet-optimized.tflite");
@@ -66,6 +72,8 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
                 swapCamera();
             }
         });
+
+        singleThreadExecutor.execute(runnable);
     }
 
     /**
@@ -110,34 +118,22 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
      */
     @Override
     public Mat onCameraFrame(Mat inputFrame) {
-        MatOfRect faces = model.detectAllFaces(inputFrame);
+        // Set and get synchronized data.
+        setFrame(inputFrame);
+        MatOfRect faces = getFaces();
 
-        // Draw rectangle for each face found in photo
-        for (Rect face : faces.toArray()) {
-            Imgproc.rectangle(
-                    inputFrame,                                                 // Image
-                    new Point(face.x, face.y),                                  //p1
-                    new Point(face.x + face.width, face.y + face.height),//p2
-                    new Scalar(0, 0, 255),                                     //color
-                    5                                                //Thickness
-            );
-        }
-
-        // TODO: Some way to track face, make some noise detection,
-        // TODO: maybe some number of frames with similar object?
-
-        // TODO: we have to proceed this input and put some number to face, and apply
-        // TODO: face detection for frame operation.
-
-        //TODO maybe some operation should be done in async, but idk.
-
-        //Pre process all images
-        ArrayList<Mat> faceImages = model.preProcessAllFaces(inputFrame, faces);
-
-        // Calculate vector for each face
-        for( Mat face :faceImages) {
-            TensorImage image = model.changeImageRes(face);
-            model.processImage(image);
+        // Until we will proceed first image, we can't proceed results.
+        if(faces != null) {
+            // Draw rectangle for each face found in photo.
+            for (Rect face : faces.toArray()) {
+                Imgproc.rectangle(
+                        inputFrame,                                                 // Image
+                        new Point(face.x, face.y),                                  //p1
+                        new Point(face.x + face.width, face.y + face.height),//p2
+                        new Scalar(0, 0, 255),                                     //color
+                        5                                                //Thickness
+                );
+            }
         }
 
         return inputFrame;
@@ -168,5 +164,105 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
         mOpenCvCameraView.disableView();
         mOpenCvCameraView.setCameraIndex(CameraIndex);
         mOpenCvCameraView.enableView();
+    }
+
+    private Runnable runnable = new Runnable() {
+        /**
+         * This function will constantly get newest frame and proceed it with face detection
+         * and recognition.
+         */
+        @Override
+        public void run() {
+            while(true)
+            {
+                // Get frame. Set frame to null to avoid doing same operation twice.
+                Mat inputFrame = getFrame();
+                setFrame(null);
+
+                if(inputFrame != null) {
+                    MatOfRect faces = model.detectAllFaces(inputFrame);
+                    // TODO: Some way to track face, make some noise detection,
+                    // TODO: maybe some number of frames with similar object?
+
+                    // TODO: we have to proceed this input and put some number to face, and apply
+                    // TODO: face detection for frame operation.
+
+                    //Pre process all images
+                    ArrayList<Mat> faceImages = model.preProcessAllFaces(inputFrame, faces);
+
+                    // Calculate vector for each face
+                    for (Mat face : faceImages) {
+                        TensorImage image = model.changeImageRes(face);
+                        model.processImage(image);
+                    }
+
+                    // Set result synchronized.
+                    setFaces(faces);
+                }
+                else
+                {
+                    // In case of no ready frame, wait some time
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
+
+    /**
+     * Synchronized function to set last frame.
+     * @param lastFrame last frame recieved from video
+     */
+    public synchronized void setFrame (Mat lastFrame)
+    {
+        this.lastFrame = lastFrame;
+    }
+
+    /**
+     * Synchronized function to get last frame.
+     * @return last frame of video
+     */
+    public synchronized Mat getFrame ()
+    {
+        return lastFrame;
+    }
+
+    /**
+     * Synchronized function to set all detected faces.
+     * @param newFaces Array of Rect, which determines face position
+     */
+    public synchronized void setFaces (MatOfRect newFaces)
+    {
+        faceMat = newFaces;
+    }
+
+    /**
+     * Synchronized function to get detected faces.
+     * @return Array of Rect, which determines face position
+     */
+    public synchronized MatOfRect getFaces ()
+    {
+        return faceMat;
+    }
+
+    /**
+     * Synchronized function to set all names of detected faces.
+     * @param names names of people on image
+     */
+    public synchronized void setNames (String[] names)
+    {
+        this.names = names;
+    }
+
+    /**
+     * Synchronized function to get all names of detected faces.
+     * @return names of people on image
+     */
+    public synchronized String[] getnames ()
+    {
+        return names;
     }
 }

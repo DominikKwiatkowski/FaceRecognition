c60d4abd90307514a36f8271ded57b9c4ed66290
Qubaef <qubaefg@gmail.com>
2021-10-02 16:29:57 +0200

    Fix database test

--- app/src/androidTest/java/com/UserDatabase/UserDatabaseTests.java
+++ app/src/androidTest/java/com/UserDatabase/UserDatabaseTests.java
@@ -6,10 +6,13 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.R;
 import com.libs.globaldata.GlobalData;
+import com.libs.globaldata.ModelObject;
 import com.libs.globaldata.userdatabase.UserDatabase;
 import com.libs.globaldata.userdatabase.UserRecord;
 
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -19,16 +22,24 @@ public class UserDatabaseTests {
     public void saveAndLoadTest() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        // Sample instrumented databse test to present functionality - will redo later
-        UserDatabase userDatabase = GlobalData.getModel(appContext,
+        // Sample instrumented database test to present functionality - will redo later
+        ModelObject modelObject = GlobalData.getModel(appContext,
                 appContext.getResources().getString(R.string.model_Facenet),
-                appContext.getResources().getString(R.string.model_filename_Facenet)).userDatabase;
+                appContext.getResources().getString(R.string.model_filename_Facenet));
 
-        userDatabase.addUserRecord(new UserRecord("Test", new float[]{(float) 13.37}));
+        UserDatabase userDatabase = modelObject.userDatabase;
+
+        Random random = new Random();
+        float[] vector = new float[modelObject.neuralModel.getOutputSize()];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = random.nextFloat();
+        }
+
+        userDatabase.addUserRecord(new UserRecord("user", vector));
         userDatabase.saveDatabase();
         userDatabase.loadDatabase();
         String[] users = userDatabase.getUsersArray();
 
-        assertArrayEquals(users, new String[]{"Test"});
+        assertArrayEquals(users, new String[]{"user"});
     }
 }

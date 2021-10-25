0a80a06acb0934a58fa26f1b6cbf2c85a0d75fd4
Dominik Kwiatowski <domik.rex@gmail.com>
2021-10-25 23:38:51 +0200

    Upgrade add photo layout.

--- app/src/main/java/com/libs/benchmark/AddPhotoLayout.java
+++ app/src/main/java/com/libs/benchmark/AddPhotoLayout.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -27,10 +28,13 @@ public class AddPhotoLayout implements LayoutClassInterface{
     private ArrayList<Bitmap> tempPhotos = new ArrayList<>();
 
     private ImageView addPhoto_ImageView;
-
+    private Button leftButton;
+    private Button rightButton;
+    private Button deletePhotoButton;
     private final ActivityResultLauncher<Intent> choosePhotoLauncher;
 
     private final ActivityResultLauncher<Intent> takePhotoLauncher;
+    private int indexer = 0;
 
     public AddPhotoLayout(AppCompatActivity caller, LayoutClassInterface benchmarkLayout){
         this.caller = caller;
@@ -67,29 +71,33 @@ public class AddPhotoLayout implements LayoutClassInterface{
     @Override
     public void makeActive() {
         caller.setContentView(R.layout.activity_benchmark_add_photo);
-        Button cancel = caller.findViewById(R.id.benchCancel);
+        Button cancel = caller.findViewById(R.id.benchCancelButton);
         cancel.setOnClickListener(v -> cancel());
 
-        Button add = caller.findViewById(R.id.benchAdd);
+        Button add = caller.findViewById(R.id.benchAddButton);
         add.setOnClickListener(v -> addTestPhotos());
 
-        Button chosePhoto = caller.findViewById(R.id.benchChoosePhoto);
+        Button chosePhoto = caller.findViewById(R.id.benchChoosePhotoButton);
         chosePhoto.setOnClickListener(v -> chosePhoto());
 
-        Button makePhoto = caller.findViewById(R.id.benchMakePhoto);
+        Button makePhoto = caller.findViewById(R.id.benchMakePhotoButton);
         makePhoto.setOnClickListener(v -> makePhoto());
 
+        leftButton = caller.findViewById(R.id.leftButton);
+        leftButton.setOnClickListener(v -> previousPhoto());
+        rightButton = caller.findViewById(R.id.rightButton);
+        rightButton.setOnClickListener(v -> nextPhoto());
         addPhoto_ImageView = caller.findViewById(R.id.benchSelectedImage);
+
+        deletePhotoButton = caller.findViewById(R.id.deletePhotoButton);
+        deletePhotoButton.setOnClickListener(v -> deletePhoto());
+
+        updateUI();
     }
 
     private void cancel(){
-        if(tempPhotos.size()==0) {
-            benchmarkLayout.makeActive();
-        }
-        else{
-            tempPhotos.remove(tempPhotos.size()-1);
-            displayPhoto();
-        }
+        tempPhotos.clear();
+        benchmarkLayout.makeActive();
     }
 
     private void addTestPhotos(){
@@ -113,15 +121,49 @@ public class AddPhotoLayout implements LayoutClassInterface{
 
     private void addNewTempPhoto(Bitmap image){
         tempPhotos.add(image);
-        displayPhoto();
+        indexer = tempPhotos.size()-1;
+        updateUI();
     }
 
-    private void displayPhoto(){
+    private void updateUI(){
         if(tempPhotos.size()>0) {
-            addPhoto_ImageView.setImageBitmap(tempPhotos.get(tempPhotos.size() - 1));
+            addPhoto_ImageView.setImageBitmap(tempPhotos.get(indexer));
+            deletePhotoButton.setVisibility(View.VISIBLE);
         }
         else{
             addPhoto_ImageView.setImageResource(0);
+            deletePhotoButton.setVisibility(View.INVISIBLE);
         }
+
+        if(indexer == 0){
+            leftButton.setVisibility(View.INVISIBLE);
+        }
+        else{
+            leftButton.setVisibility(View.VISIBLE);
+        }
+        if(indexer >= tempPhotos.size() - 1){
+            rightButton.setVisibility(View.INVISIBLE);
+        }
+        else{
+            rightButton.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void nextPhoto(){
+        indexer++;
+        updateUI();
+    }
+
+    private void previousPhoto(){
+        indexer--;
+        updateUI();
+    }
+
+    private void deletePhoto(){
+        tempPhotos.remove(indexer);
+        if(indexer > 0)
+            indexer--;
+        updateUI();
     }
 }

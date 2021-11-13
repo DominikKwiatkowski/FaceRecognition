3339c0714c5e15b89e2b8bd27ebd170edc64ffb6
Dominik Kwiatowski <domik.rex@gmail.com>
2021-11-13 11:28:33 +0100

    Add tests for naural model and image preprocessor.

--- app/src/androidTest/java/com/PreProcessorTest.java
+++ app/src/androidTest/java/com/PreProcessorTest.java
@@ -14,13 +14,18 @@ import com.google.mlkit.vision.face.Face;
 import com.libs.facerecognition.FacePreprocessor;
 import com.libs.globaldata.GlobalData;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -28,14 +33,23 @@ import static org.junit.Assert.assertTrue;
  * of using different methods.
  */
 @RunWith(AndroidJUnit4.class)
-public class FindFaceTest {
+public class PreProcessorTest {
 
     // Number of margin percentage
     private static final int margin = 10;
     private static final int testCases = 2;
 
     private FacePreprocessor preProcessor;
+    private Resources res;
+    private Context appContext;
 
+    public PreProcessorTest()
+    {
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        res = appContext.getResources();
+        // Name of model does not have any impact on results, it is never used.
+        preProcessor = GlobalData.getFacePreProcessor();
+    }
     /**
      * Function to check if results are correct. We use margin, which is equal to margin field.
      * It is only possible to test photo with one face on it.
@@ -88,13 +102,10 @@ public class FindFaceTest {
 
     @Test
     /**
-     * Check if face detector found the face on the image and if position of face is close to where it should be.
+     * Check if face detector found the face on the image and if position of face is close to where
+     * it should be.
      */
-    public void performTest() {
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        Resources res = appContext.getResources();
-        // Name of model does not have any impact on results, it is never used.
-        preProcessor = GlobalData.getFacePreProcessor();
+    public void FindFaceTest() {
 
         // Expected Results
         Rect[] faces = new Rect[testCases];
@@ -111,5 +122,47 @@ public class FindFaceTest {
         for (int i = 0; i < testCases; i++) {
             assertTrue(photoIsCorrect(images[i], faces[i]));
         }
+    }
+
+    @Test
+    /**
+     * Testing if face on image works correctly.
+     */
+    public void IsFaceOnImageTest(){
+        Bitmap faceNotInImagePhoto = BitmapFactory.decodeResource(res, R.drawable.face_not_in_image);
+        Bitmap faceInImagePhoto = BitmapFactory.decodeResource(res, R.drawable.face_in_image);
+
+        Task<List<Face>> faceOutsideImage = preProcessor.detectAllFacesUsingML(faceNotInImagePhoto);
+        Task<List<Face>> faceInsideImage = preProcessor.detectAllFacesUsingML(faceInImagePhoto);
+
+        preProcessor.waitForTask(faceOutsideImage);
+        preProcessor.waitForTask(faceInsideImage);
+
+        assertFalse(preProcessor.isFaceOnImage(faceOutsideImage.getResult().get(0), faceNotInImagePhoto));
+        assertTrue(preProcessor.isFaceOnImage(faceInsideImage.getResult().get(0), faceInImagePhoto));
+    }
+
+    @Test
+    /**
+     * Check rotate and trim face works correctly.
+     */
+    public void rotateAndTrimFace(){
+        Bitmap faceInImagePhoto = BitmapFactory.decodeResource(res, R.drawable.face_in_image);
+        Task<List<Face>> faceInsideImage = preProcessor.detectAllFacesUsingML(faceInImagePhoto);
+        preProcessor.waitForTask(faceInsideImage);
+
+        Bitmap trimmed = preProcessor.rotateAndTrimFace(faceInImagePhoto, faceInsideImage.getResult().get(0));
+        Bitmap expectedTrimmed = BitmapFactory.decodeResource(res, R.drawable.face_trimmed);
+        equals(trimmed,expectedTrimmed);
+    }
+
+    private boolean equals(Bitmap bitmap1, Bitmap bitmap2) {
+        ByteBuffer buffer1 = ByteBuffer.allocate(bitmap1.getHeight() * bitmap1.getRowBytes());
+        bitmap1.copyPixelsToBuffer(buffer1);
+
+        ByteBuffer buffer2 = ByteBuffer.allocate(bitmap2.getHeight() * bitmap2.getRowBytes());
+        bitmap2.copyPixelsToBuffer(buffer2);
+
+        return Arrays.equals(buffer1.array(), buffer2.array());
     }
 }

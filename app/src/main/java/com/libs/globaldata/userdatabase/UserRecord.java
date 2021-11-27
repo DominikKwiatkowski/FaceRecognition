466d32f579cfe8c3f2ae4db225deb585daa5c326
Etav99 <etavpl@gmail.com>
2021-11-27 20:38:03 +0100

    Fix vector correection

--- app/src/main/java/com/libs/globaldata/userdatabase/UserRecord.java
+++ app/src/main/java/com/libs/globaldata/userdatabase/UserRecord.java
@@ -23,7 +23,7 @@ public class UserRecord implements Serializable {
     public void correctVector(@NonNull float[] vector) {
         if (this.vector.length == vector.length) {
             for (int i = 0; i < vector.length; i++) {
-                this.vector[i] = (this.vector[i] * weight + vector[i]) / weight + 1;
+                this.vector[i] = (this.vector[i] * weight + vector[i]) / (weight + 1);
             }
 
             // Track vector's weight for further corrections
